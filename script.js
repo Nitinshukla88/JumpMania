1e953a9432b3efc13c71b4536bf5cff4951fb24e
Nitin shukla <nitin.shukla12004@gmail.com>
2024-05-18 18:48:26 +0530

    did some more code on making the enemy appear on the screen

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 document.onkeydown= function(e){
     console.log(e.keyCode)
     if(e.keyCode==38){
-        player1 = document.querySelector('imageclass')
+        player1 = document.querySelector('.player')
         console.log(player1)
         player1.classList.add('animatedplayer')
+        setTimeout(() => {
+            player1.classList.remove('animatedplayer')
+        }, 700);
+        enemy= document.querySelector('.enemy')
+        enemy.classList.add('animatedenemy')
     }
 }
