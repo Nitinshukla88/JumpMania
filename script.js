1d7d2d2328f293d2b30ff410206ff06a25f38508
Nitin shukla <nitin.shukla12004@gmail.com>
2024-05-19 22:59:00 +0530

    Added sound effects in the game

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 score=0
-canjump = true;
+canjump = true
+
+play = true
+
+gamesound = new Audio('gamesound.mp3')
+gameoversound = new Audio('gameoversound.mp3')
 document.onkeydown= function(e){
+    setTimeout(() => {
+        if(play==true){
+            gamesound.play()
+        }
+    }, 200);
     console.log(e.keyCode)
     if(e.keyCode==38){
         player1 = document.querySelector('.player')
@@ -36,6 +46,10 @@ setInterval(() => {
 
     if(diffX<50 && diffY<52){
         Enemy.classList.remove('movingenemy')
+        gamesound.pause()
+        play = false
+        gameoversound.play()
+
     }else if(diffX<130 && canjump){
         score+=1
         displayscore(score)
@@ -43,6 +57,7 @@ setInterval(() => {
         setTimeout(() => {
             canjump  = true
         }, 1000);
+
     }
 }, 10);
 
